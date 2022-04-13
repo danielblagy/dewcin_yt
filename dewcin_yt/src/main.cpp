512b96acc2ecdb46b38d2c46300ecbab64825843
Daniel Blagy <daniel.blagy@gmail.com>
2022-04-13 17:24:44 +0300

    ep6 using delta

--- dewcin_yt/src/main.cpp
+++ dewcin_yt/src/main.cpp
@@ -4,34 +4,44 @@ dewcin_app_entry_point
 {
 	// game init code
 
-	int x = 250, y = 250;
+	float x = 250.0f, y = 250.0f;
 	
 	dewcin::Game::setGameUpdate([&](float delta)
 	{
 		wchar_t charBuffer[256];
-		swprintf(charBuffer, 256, L"delta: %f\n", delta);
-		OutputDebugString(charBuffer);
+		//swprintf(charBuffer, 256, L"delta: %f\n", delta);
+		//OutputDebugString(charBuffer);
+
+		static int frames = 0;
+		static float timePassed = 0.0f;
+
+		frames++;
+		timePassed += delta;
+
+		if (timePassed >= 1.0f)
+		{
+			swprintf(charBuffer, 256, L"FPS: %d\n", frames);
+			OutputDebugString(charBuffer);
+
+			timePassed -= 1.0f;
+			frames = 0;
+		}
 
 		dewcin::Renderer::SetPixel(10, 10, { 0, 0, 255 });
 		
-		dewcin::Renderer::FillRectangle({ x, y, 320, 180 }, { 0, 255, 0 });
+		// x = 3.4 = 3	x = 3.4 + 0.5 = 3.9 -> 3
+		// x = 3.8 = 3  x = 3.8 ) 0.5 = 4.3 -> 4
+		dewcin::Renderer::FillRectangle({ int(x + 0.5f), int(y + 0.5f), 320, 180 }, { 0, 255, 0 });
 
-		if (dewcin::Input::isKeyPressed(DC_UP))
-			y -= 10;
-		else if (dewcin::Input::isKeyPressed(DC_DOWN))
-			y += 10;
+		if (dewcin::Input::isKeyPressed(DC_W))
+			y -= 100.0f * delta;
+		else if (dewcin::Input::isKeyPressed(DC_S))
+			y += 100.0f * delta;
 
-		if (dewcin::Input::isKeyPressed(DC_LEFT))
-			x -= 10;
-		else if (dewcin::Input::isKeyPressed(DC_RIGHT))
-			x += 10;
-
-		if (dewcin::Input::isMouseButtonPressed(DC_MOUSE_MIDDLE))
-			OutputDebugString(L"mouse middle button pressed!\n");
-
-		dewcin::Input::Position mousePosition = dewcin::Input::getMousePosition();
-		swprintf(charBuffer, 256, L"%d, %d\n", mousePosition.x, mousePosition.y);
-		OutputDebugString(charBuffer);
+		if (dewcin::Input::isKeyPressed(DC_A))
+			x -= 100.0f * delta;
+		else if (dewcin::Input::isKeyPressed(DC_D))
+			x += 100.0f * delta;
 	}
 	);
 
